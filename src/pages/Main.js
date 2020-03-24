40202494ca1d151ce4400d9d411c3f07d2d0399e
Vlad Temian <vladtemian@gmail.com>
2020-03-24 16:50:25 +0200

    POC for rendering

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,4 +1,5 @@
 import React, { Fragment } from "react";
+import { PDFDownloadLink, Document, Page, View, StyleSheet, Font, Text as Content } from '@react-pdf/renderer'
 import { createGlobalStyle } from "styled-components";
 
 import { useSetState } from "../helpers/hooks";
@@ -9,6 +10,7 @@ import Title from "../components/Title";
 import Input from "../components/Input";
 import Wrapper from "../components/Wrapper";
 import Container from "../components/Container";
+import Renderer from "../components/Renderer";
 
 const CSSReset = createGlobalStyle`
   html, body, p, ol, ul, li, hr, h1, h2, h3, h4, h5, h6 {
@@ -40,6 +42,36 @@ const CSSReset = createGlobalStyle`
   }
 `;
 
+Font.register({
+  family: 'Oswald',
+  src: 'https://fonts.gstatic.com/s/oswald/v13/Y_TKV6o8WovbUd3m_X9aAA.ttf'
+});
+
+const styles = StyleSheet.create({
+  body: {
+    paddingTop: 35,
+    paddingBottom: 65,
+    paddingHorizontal: 35,
+  },
+  title: {
+    fontSize: 16,
+    textAlign: 'center',
+    fontFamily: 'Times-Roman'
+  },
+  text: {
+    margin: 12,
+    fontSize: 14,
+    textAlign: 'justify',
+    fontFamily: 'Times-Roman'
+  },
+  header: {
+    fontSize: 12,
+    marginBottom: 20,
+    textAlign: 'center',
+    color: 'grey',
+  },
+});
+
 function Main() {
   const [form, setForm] = useSetState({
     nume: undefined,
@@ -63,6 +95,18 @@ function Main() {
     });
   };
 
+  const Declaration = ({context}) => (
+    <View>
+        <Content style={styles.title}>Declarație pe proprie răspundere</Content>
+        <Content style={styles.text}>
+          Subsemnatul(a) {context.nume}, fiul/fiica lui {context.tata} și al
+          {context.mama}, domiciliat(ă) în {context.oras}, județ/sector {context.judet}, strada {context.strada}, număr
+          {context.numar}, bloc {context.bloc}, etaj {context.etaj}, apartament
+          {context.apartament}, având CNP {context.cnp}, BI/CI seria {context.ci_seria}, numar {context.ci_numar}.
+        </Content>
+    </View>
+  );
+
   return (
     <Fragment>
       <CSSReset />
@@ -84,6 +128,22 @@ function Main() {
           <Input name="ci_seria" placeholder="TM" value={form.ci_seria} onChange={onChange} />, numar
           <Input name="ci_numar" placeholder="123456" value={form.ci_numar} onChange={onChange} />.
         </Wrapper>
+
+        <PDFDownloadLink document={
+          <Document>
+            <Page size={"A4"}>
+              <Declaration context={form} />
+            </Page>
+          </Document>
+        } fileName={`declaratie-${form.nume}`}>
+          {({ loading }) => (loading ? 'Loading document...' : 'Download now!')}
+        </PDFDownloadLink>
+
+      </Container>
+      <Container>
+        <Renderer>
+          <Declaration context={form}/>
+        </Renderer>
       </Container>
     </Fragment>
   );
